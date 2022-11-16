01f42bc40da925f46b3fb6dc08a235132d01852e
mohamad9999 <mohtashamimohamad99@gmail.com>
2022-11-16 20:13:05 +0300

    third

--- layouts/App.js
+++ layouts/App.js
@@ -1,26 +1,47 @@
 import { StyleSheet,Image, Text, View } from 'react-native';
 
 export default function App() {
-  var myloop = []
-  for (let i = 0; i < 8; i++) {
-    myloop.push(
-      <View style={styles.child}>
-        <Image
-        style={styles.image}
-        source={{
-          uri: 'https://img.freepik.com/vecteurs-libre/homme-affaires-caractere-avatar-isole_24877-60111.jpg?w=2000'
-        }}
-          />
-        <Text style={styles.text}>Lorem Ipsum is simply dummy text of the printing and typesetting industry</Text>
-      </View>
-    );
-  }
   
   return (
     <View style={styles.container}>
-      {
-        myloop
-      }
+      <View style={styles.name}>
+        <View style={{flexDirection: 'row', }}>
+          <Image
+          style={{width:30, height:30, borderRadius: 30}}
+          source={{
+            uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyHtT01NkPRmYezUuwD2nQIdMvzWrVLHpdhg&usqp=CAU'
+          }}></Image>
+          <Text style={{color: 'white',marginLeft: 40}}>whatsapp call</Text>
+        </View>
+        <Text style={{fontSize: 25, color: 'white'}}>Mark Johnson</Text>
+        <Text style={{color: 'white'}}>CALLING</Text>
+      </View>
+      <Image 
+      style={styles.image}
+      source={{
+        uri: 'https://cdn-icons-png.flaticon.com/512/147/147138.png'
+      }}
+      ></Image>
+      <View style={styles.call}>
+        <Image
+        style={{width:35, height:35, borderRadius: 30}}
+        source={{
+          uri: 'https://www.creativefabrica.com/wp-content/uploads/2021/04/18/Audio-speaker-icon-Graphics-11037028-1-580x386.jpg'
+        }}
+        ></Image>
+        <Image
+        style={{width:50, height:50, borderRadius: 30}}
+        source={{
+          uri: 'https://w7.pngwing.com/pngs/654/892/png-transparent-end-call-logo-telephone-number-email-telephone-call-payphone-telefono-miscellaneous-text-trademark.png'
+        }}
+        ></Image>
+        <Image
+        style={{width:35, height:35, borderRadius: 30}}
+        source={{
+          uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7o5ne7NdPLozDRLqJ61ouPhQU1sFPHn13_w&usqp=CAU'
+        }}
+        ></Image>
+      </View>
     </View>
   );
 }
@@ -28,27 +49,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'lightgray',
+    backgroundColor: '#1d4e33',
   },
-  child: {
-    flex: 1,
-    backgroundColor: '#fff',
-    width: "100%",
-    margin: 4 ,
-    borderRadius: 20,
-    alignItems: 'center',
-    flexDirection: "row"
+  name: {
+    flex: 1.5,
+    justifyContent: 'center',
+    paddingLeft: 20
+
   },
   image: {
-    width:50,
-    height: 50,
-    borderRadius: 30,
-    margin: 10
+    flex: 3,
   },
-  text: {
-    color: 'blue', 
-    fontSize: 18, 
-    width: '70%', 
-    marginLeft: 6
+  call: {
+    flex: 3,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
   }
 });
